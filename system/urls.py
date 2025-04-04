b0034f97ab3aa4a401f9329159b1130581807fc0
LeKhanhThien0302 <thienpro1356@gmail.com>
2025-04-04 15:33:41 +0700

    upload

--- system/urls.py
+++ system/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'), 
     path('home/', views.home, name='home'),
     path('login/',views.login,name='login'),
+    path('vehicle_entry',views.vehicle_entry,name='vehicle_entry'),
+    path('parking_status',views.parking_status,name='parking_status'),
 ]
--- system/views.py
+++ system/views.py
@@ -7,3 +7,7 @@ def login(request):
     return render(request,'login.html')
 def register(request):
     return render(request,'register.html')
+def vehicle_entry(request):
+    return render(request,'vehicle_entry.html')
+def parking_status(request):
+    return render(request,'parking_status.html')
