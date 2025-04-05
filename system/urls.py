b5383b1fa25709d0010728c8367dbe248e6e350e
QuocAnh-1 <24242424vvaa@gmail.com>
2025-04-05 16:50:13 +0700

    upload

--- system/urls.py
+++ system/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('vehicle_management/',views.vehicle_management,name='vehicle_management'),
     path('parking_status/',views.parking_status,name='parking_status'),
     path('vehicle_report/', views.vehicle_report, name='vehicle_report'),
+    path('register/', views.register, name='register'),
+    
 ]
