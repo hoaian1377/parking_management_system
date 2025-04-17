d28df441f38dd9b07893b9b6d613b6a042a41269
LeKhanhThien0302 <thienpro1356@gmail.com>
2025-04-17 16:31:06 +0700

    upalod

--- system/urls.py
+++ system/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'), 
-    path('home/', views.home, name='home'),
+    path('', views.login, name='login'), 
     path('login/',views.login,name='login'),
+    path('home/', views.home, name='home'),
     path('vehicle_management/',views.vehicle_management,name='vehicle_management'),
     path('parking_status/',views.parking_status,name='parking_status'),
     path('vehicle_report/', views.vehicle_report, name='vehicle_report'),
