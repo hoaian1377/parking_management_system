1623593ced3f719d2e8e57db94bba4de3ceb2d88
Tin Loc~ <trantinloc2005@gmail.com>
2025-04-20 14:15:58 +0700

    uplaod lại login

--- system/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class PasswordResetRequestForm(forms.Form):
-    email = forms.EmailField(label="Email", max_length=254, widget=forms.EmailInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Nhập email của bạn'
-    }))
