7a482f5f71acb741d2541e3d281eeb876d5479dd
Tin Loc~ <trantinloc2005@gmail.com>
2025-04-26 22:37:51 +0700

    upload

--- parking_system/settings.py
+++ parking_system/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'parking_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'mssql',
-        'NAME': 'QLHTX2', 
+        'NAME': 'QLBX', 
         'USER': '',
         'PASSWORD': '',
-        'HOST': 'DESKTOP-8LHJJ0H',
+        'HOST': 'TIN-LOC',
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server',
             'trusted_connection':'yes', 
